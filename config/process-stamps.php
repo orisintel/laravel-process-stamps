bb93a3415fe0415a96e6cfc1683c3718836d3075
Tom Schlick <tschlick@orisintel.com>
2018-09-04 11:44:58 -0400

    added config file

---
+++ config/process-stamps.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'table' => 'process_stamps',
+
+    'columns' => [
+        'created' => 'process_created_id',
+        'updated' => 'process_updated_id',
+    ],
+
+    'cache' => [
+        'enabled' => true,
+        'store' => env('PROCESS_STAMP_CACHE_DRIVER', 'redis'),
+    ],
+
+];
