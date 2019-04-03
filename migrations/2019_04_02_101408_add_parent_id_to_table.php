b600abe6c9f40ed7c63dbc6464990b75dd631d3d
Tom Schlick <tschlick@orisintel.com>
2019-04-03 16:20:12 -0400

    add fk

--- migrations/2019_04_02_101408_add_parent_id_to_table.php
+++ migrations/2019_04_02_101408_add_parent_id_to_table.php
@@ -14,7 +14,11 @@ class AddParentIdToTable extends Migration
     public function up()
     {
         Schema::table(config('process-stamps.table'), function (Blueprint $table) {
-            $table->unsignedInteger('parent_id')->nullable()->index();
+            $table->unsignedInteger('parent_id')->nullable()->index()->after('hash');
+
+            $table->foreign('parent_id')
+                ->references(config('process-stamps.columns.primary_key'))
+                ->on(config('process-stamps.table'));
         });
     }
 
