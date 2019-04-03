c89005e248fb3e0fd6716a17874b99b628910245
Tom Schlick <tschlick@orisintel.com>
2019-04-03 13:01:30 -0400

    Post model for testing

---
+++ tests/Fakes/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace OrisIntel\ProcessStamps\Tests\Fakes;
+
+
+use Illuminate\Database\Eloquent\Model;
+use OrisIntel\ProcessStamps\ProcessStampable;
+
+class Post extends Model
+{
+    use ProcessStampable;
+
+    public $table = 'posts';
+    public $guarded = [];
+}
