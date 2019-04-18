2db9f6fe2030cbb851546a895335a485c8da6551
Tom Schlick <tom@schlick.email>
2019-04-18 12:23:00 -0400

    only add parent_id column if it doesn't exist (#8)
    * only add parent_id column if it doesn't exist
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * explanation
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]

--- migrations/2018_09_04_161009_add_parent_id_to_table.php
+++ migrations/2018_09_04_161009_add_parent_id_to_table.php
@@ -14,11 +14,16 @@ class AddParentIdToTable extends Migration
     public function up()
     {
         Schema::table(config('process-stamps.table'), function (Blueprint $table) {
-            $table->unsignedInteger('parent_id')->nullable()->index()->after('hash');
 
-            $table->foreign('parent_id')
-                ->references(config('process-stamps.columns.primary_key'))
-                ->on(config('process-stamps.table'));
+            // In v1.1.2 we changed the timestamp of this migration.
+            // This check prevents issues if someone already migrated with the old timestamp.
+            if (! Schema::hasColumn(config('process-stamps.table'), 'parent_id')) {
+                $table->unsignedInteger('parent_id')->nullable()->index()->after('hash');
+
+                $table->foreign('parent_id')
+                    ->references(config('process-stamps.columns.primary_key'))
+                    ->on(config('process-stamps.table'));
+            }
         });
     }
 
